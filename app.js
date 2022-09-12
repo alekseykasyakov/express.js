b0a129745a99b07ef9b40614bac577fe4485c2be
Aleksey <alekseykasyakov@gmail.com>
2022-09-12 15:59:25 -0700

    parse data from form

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use('/public',express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'static','index.html'));
 });
-app.get('/example',(req,res)=>{
-    res.send('hitting example route');
+
+app.post('/', (req,res)=>{
+    console.log(req.body);
+    // database work here
+    res.send('successdully posted data');
 });
-app.get('/example/:name/:age',(req,res)=>{
-    console.log(req.params);
-    console.log(req.query);
-    res.send(req.params.name + " : " + req.params.age);
-});
+// app.get('/example',(req,res)=>{
+//     res.send('hitting example route');
+// });
+// app.get('/example/:name/:age',(req,res)=>{
+//     console.log(req.params);
+//     console.log(req.query);
+//     res.send(req.params.name + " : " + req.params.age);
+// });
 
 app.listen(3000);
