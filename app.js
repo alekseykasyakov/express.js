4107201bccc360749640e2a56095c33f23dc1b57
Aleksey <alekseykasyakov@gmail.com>
2022-09-12 22:37:20 -0700

    return json in array object from form

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use('/public',express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'static','index.html'));
@@ -14,7 +14,8 @@ app.get('/',(req,res)=>{
 app.post('/', (req,res)=>{
     console.log(req.body);
     // database work here
-    res.send('successdully posted data');
+    //res.send('successdully posted data');
+    res.json({success : true});
 });
 // app.get('/example',(req,res)=>{
 //     res.send('hitting example route');
